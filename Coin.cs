b396d7697a05e19c969967d100a868ab555834c6
jennyf19 <ferriesj@gmail.com>
2017-01-23 18:53:18 -0800

    start of coin class

---
+++ Coin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Coin
+    {
+        public enum Denomination
+        { SLUG = 0, NICKEL = 5, DIME = 10, QUARTER = 25 }
+        public Coin() { }
+
+        public Coin(Denomination CoinEunmeral)
+        {
+
+        }
+
+        public Coin(string CoinName)
+        {
+            Denomination coinEnumeral;
+            if(Enum.)
+        }
+
+        public decimal ValueOf
+        { }
+    }
+}
